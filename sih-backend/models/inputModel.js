49d592f6416c02ed76834c3dfd5f98b4c03bd044
Bhoirob2004 <bhoirobgogoi23@gmail.com>
2025-10-17 01:11:26 +0530

    Input Schema is created

---
+++ sih-backend/models/inputModel.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const inputSchema = mongoose.Schema({
+    location : {
+        type : String,
+        required : [true, "Location is mandatory"]
+    },
+    period : {
+        type : String,
+        required : [true, "Period is mandatory"]
+    },
+    soilType : {
+        type : String,
+        required : false
+    },
+    cropPreferance :{
+        type : String,
+        required : false
+    },
+    irrigation :{
+        type : String,
+        required : [true, "irrigation is mandatory"]
+    }
+}, {timeStamps : true})
+
+module.exports = mongoose.model("Input", inputSchema);
+
+ 
+//estrict possible values (for dropdowns in frontend), you can add enums:
+
+// soilType: {
+//   type: String,
+//   enum: ["Loamy", "Clay", "Sandy", "Silty"],
+//   required: false,
+// },
+// irrigation: {
+//   type: String,
+//   enum: ["Rainfed", "Canal", "Drip", "Well"],
+//   required: [true, "Irrigation is mandatory"],
+// }
